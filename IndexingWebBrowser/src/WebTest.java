4121a6c3ceba31802b969640796cba02724c0553
Curtis <Zhuangyongj@gmail.com>
2019-02-11 21:55:44 +0000

    Brief details about this program

---
+++ IndexingWebBrowser/src/WebTest.java
@@ -0,0 +1,13 @@
+/**
+ * This class will be used to load the web documents(files or URLs) indicated in a text file,
+ * and then produce the summary statistics about these web documents.
+ * Formats of statistics: 
+ * [source] [word count] [alphabetic range of content words] [number of distinct keywords] [evaluation of the HTML format]
+ * @author 180139796 
+ * @version 1.1
+ * Created on 11/01/2019
+ *
+ */
+public class WebTest {
+
+}
