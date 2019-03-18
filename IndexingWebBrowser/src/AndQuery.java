7b09ba0a58375a997835e99ad09032a5a2097f91
Curtis <Zhuangyongj@gmail.com>
2019-03-18 17:13:22 +0000

    Building the basic hierarchical structure for feature_2

---
+++ IndexingWebBrowser/src/AndQuery.java
@@ -0,0 +1,11 @@
+import java.util.Set;
+
+public class AndQuery implements Query {
+
+	@Override
+	public Set<WebDoc> matches(WebIndex wind) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ IndexingWebBrowser/src/OrQuery.java
@@ -0,0 +1,11 @@
+import java.util.Set;
+
+public class OrQuery implements Query{
+
+	@Override
+	public Set<WebDoc> matches(WebIndex wind) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
