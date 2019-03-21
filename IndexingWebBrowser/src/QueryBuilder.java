9f0c7c724f87f4daa173262fc99e6673b995767c
Curtis <Zhuangyongj@gmail.com>
2019-03-21 23:40:52 +0000

    Implemented parseInfixForm()
    A number of methods that support parsing the Infix form of query are
    added, where the parseInfixString(String str) and the
    parseCoveringBracket(String q) are the recursive methods.
    
    The parseAndInfix(String left, String right), parseOrInfix(String left,
    String right), and the parseNotInfix(String right) are called to process
    the queries, they will call back to the parseInfixString(String str)
    recursively until the query is atomic or a NotQuery.

--- IndexingWebBrowser/src/QueryBuilder.java
+++ IndexingWebBrowser/src/QueryBuilder.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Stack;
 import java.util.TreeSet;
 import java.util.regex.Matcher;
@@ -60,209 +61,93 @@ public class QueryBuilder {
 		return subQuery;
 	}
 
-//	// return query
-//	public static Query parseInfixForm(String q) {
-//		String wholeQuery = q.toLowerCase();
-//		StringBuilder parsedQuery = new StringBuilder();
-//
-//		if (wholeQuery.startsWith("(") && wholeQuery.endsWith(")")) {
-//			wholeQuery = wholeQuery.substring(1, wholeQuery.length() - 1);
-//		}
-//		// extract the operator in sequence
-//		ArrayList<String> operators = extractOperator(wholeQuery);
-//		// split the order by operators
-//		ArrayList<String> splitedQuery = splitQuery(wholeQuery);
-//
-//		Stack<Character> brackets = new Stack<>();
-//		ArrayList<String> combinedQuery = new ArrayList<>();
-//		for (int x = 0; x < operators.size(); x++) {
-//			if (operators.get(x).equals("not")) {
-//				if (x == 0) {
-//					combinedQuery.add("not" + splitedQuery.get(x));
-//				} else {
-//					combinedQuery.add("not" + splitedQuery.get(x + 1));
-//				}
-//			} else {
-//				if (splitedQuery.get(x).contains("(")) {
-//					// Found '(' and push them into the stack
-//					for (int y = 0; y < splitedQuery.get(x).length(); y++) {
-//						if (splitedQuery.get(x).charAt(y) == '(') {
-//							brackets.push('(');
-//						}
-//					}
-//					// Check the other splited query and see if there is ')'
-//					int otherSplitedQuery = x;
-//					boolean hasBracket = true;
-//					while (!hasBracket && otherSplitedQuery < splitedQuery.size()) {
-//						otherSplitedQuery++;
-//						char[] splitedQueryChar = splitedQuery.get(otherSplitedQuery).toCharArray();
-//						for (char c : splitedQueryChar) {
-//							if (c == ')') {
-//								brackets.pop();
-//							} else if (c == '(') {
-//								brackets.push('(');
-//							}
-//						}
-//						splitedQuery.set(x,
-//								splitedQuery.get(x) + operators.get(x) + splitedQuery.get(otherSplitedQuery));
-//						splitedQuery.remove(otherSplitedQuery);
-//						if (brackets.isEmpty()) {
-//							hasBracket = false;
-//						}
-//					}
-//				}
-//			}
-//		}
-//		return splitedQuery.toString();
-//	}
-
-	// used recursion
-	public static String extractInfixQuery(String q) {
-		String wholeQuery = q.toLowerCase();
-		wholeQuery = wholeQuery.replaceAll("\\s+", " "); // avoid multiple whitespace
-		StringBuilder finalResult = new StringBuilder();
-		char[] wholeQueryChar = wholeQuery.toCharArray();
-		Stack<Character> brackets = new Stack<>();
-		ArrayList<String> parsedQuery = new ArrayList<>();
-
-		StringBuilder eachPart = new StringBuilder();
-		for (char c : wholeQueryChar) {
-			if (c == '(') {
-				brackets.push('(');
-				eachPart.append(c);
-			} else if (c == ')') {
-				brackets.pop();
-				if (brackets.isEmpty()) {
-					eachPart.append(c);
-					String tempString = eachPart.toString().substring(1, eachPart.length() - 1);
-					if (tempString.contains("(") || countOperators(tempString) > 1) {
-						parsedQuery.add(extractInfixQuery(eachPart.toString()));
-						eachPart = new StringBuilder();
-					} else {
-						parsedQuery.add(parseInfixToPrefix(tempString));
-						eachPart = new StringBuilder();
-					}
-				}
-			} else if (c == ' ') {
-				if (eachPart.length() == 0) {
-					// do nothing
-				} else if (eachPart.toString().matches("(and)|(not)|(or)")) { // this part is purely an operator
-					parsedQuery.add(eachPart.toString());
-					eachPart = new StringBuilder();
-				} else if (eachPart.toString().contains("(and)|(not)|(or)")) { // this part contains an operator
-					parsedQuery.add(extractInfixQuery(eachPart.toString()));
-					eachPart = new StringBuilder();
-				}
-			} else {
-				eachPart.append(c);
-			}
-		}
-		return combineParsedQuery(parsedQuery);
+	public static Query parseInfixForm(String q) {
+		String prefixQuery = parseInfix(q);
+		return parse(prefixQuery);
 	}
 
-	private static String combineParsedQuery(ArrayList<String> parsedQuery) {
-		ArrayList<String> queryCombination = new ArrayList<>();
-		System.out.println(parsedQuery.toString());
-		StringBuilder result = new StringBuilder();
-		boolean hasOperator = false;
-		for (String str : parsedQuery) {
-			if (str.matches("(and)|(or)")) {
-				hasOperator = true;
-			}
-		}
-
-		if (hasOperator) {
-			for (int x = 0; x < parsedQuery.size(); x++) {
-				if (parsedQuery.get(x).matches("(and)|(or)")) {
-					queryCombination.add(
-							parsedQuery.get(x) + "(" + parsedQuery.get(x - 1) + "," + parsedQuery.get(x + 1) + ")");
-				} else if (parsedQuery.get(x).matches("not")) {
-					queryCombination.add(parsedQuery.get(x) + "(" + parsedQuery.get(x + 1) + ")");
-				}
-			}
-			for (String str : queryCombination) {
-				result.append(str);
-			}
-		} else {
-			for (String str : parsedQuery) {
-				result.append(str);
-			}
-		}
-		return result.toString();
-	}
-
-	private static int countOperators(String tempString) {
-		Pattern operatorPattern = Pattern.compile("(and)|(not)|(or)");
-		Matcher operatorMatcher = operatorPattern.matcher(tempString);
-		int count = 0;
-		while (operatorMatcher.find()) {
-			count++;
-		}
-		return count;
-	}
-
-	public static ArrayList<String> splitQuery(String wholeQuery) {
-		// Split the Query into parts based on the operators
-		StringBuilder sb = new StringBuilder();
-		String[] splitedQuery = wholeQuery.split("(and)|(not)|(or)");
-		for (String str : splitedQuery) {
-			sb.append(str + "\n");
-		}
-		ArrayList<String> resultList = new ArrayList<>();
-		for (String str : splitedQuery) {
-			resultList.add(str);
-		}
-		return resultList;
-	}
-
-	private static ArrayList<String> extractOperator(String wholeQuery) {
-		ArrayList<String> operators = new ArrayList<>();
-		StringBuilder eachWord = new StringBuilder();
-		for (int x = 0; x < wholeQuery.length(); x++) {
-			String thisWord = eachWord.toString();
-			char thisChar = wholeQuery.charAt(x);
-			if (thisChar == ' ' || thisChar == '(' || thisChar == ')') {
-				if (thisWord.matches("(and)|(or)|(not)")) {
-					operators.add(thisWord);
-					eachWord = new StringBuilder();
-				} else {
-					eachWord = new StringBuilder();
-				}
-			} else {
-				eachWord.append(thisChar);
-			}
-		}
-		return operators;
-	}
-
-	private static String parseInfixToPrefix(String q) {
-		String subQuery = q;
-		StringBuilder newSubQuery = new StringBuilder();
-		String operator = null;
-		String leftQuery;
-		String rightQuery;
-		String notQuery;
+	private static String parseInfix(String str) {
+		String query = str.trim();
+		query = parseCoveringBracket(query);
+		Stack<Character> bracket = new Stack<>();
+		String operator = "atomic";
 		int startIndex = 0;
 		int endIndex = 0;
-		if (subQuery.contains("and")) {
-			startIndex = subQuery.indexOf("and");
-			endIndex = startIndex + 2;
-			leftQuery = subQuery.substring(0, endIndex - 2);
-			rightQuery = subQuery.substring(endIndex +1 , subQuery.length());
-			newSubQuery.append("and" + "(" + leftQuery + "," + rightQuery + ")");
-		} else if (subQuery.contains("or")) {
-			startIndex = subQuery.indexOf("or");
-			endIndex = startIndex + 1;
-			leftQuery = subQuery.substring(0, endIndex - 2);
-			rightQuery = subQuery.substring(endIndex +1 , subQuery.length());
-			newSubQuery.append("or" + "(" + leftQuery + "," + rightQuery + ")");
-		} else if (subQuery.contains("not")) {
-			startIndex = subQuery.indexOf("not");
-			endIndex = startIndex + 2;
-			notQuery = subQuery.substring(endIndex +1 , subQuery.length());
-			newSubQuery.append("not" + "(" + notQuery + ")");
+		StringBuilder stringBuilder = new StringBuilder();
+
+		if (query.startsWith("not") && !query.contains("and") && !query.contains("or")) {
+			operator = "not";
+			endIndex = 3;
+		} else if (!query.contains("and") && !query.contains("not") && !query.contains("or")) {
+			operator = "atomic";
+		} else {
+			for (int x = 0; x < query.length(); x++) {
+				char tempChar = query.charAt(x);
+				if (tempChar == ' ' && bracket.isEmpty()) {
+					if (stringBuilder.toString().contains("and")) {
+						operator = "and";
+						startIndex = x - 3;
+						endIndex = x;
+						break;
+					} else if (stringBuilder.toString().contains("or")) {
+						operator = "or";
+						startIndex = x - 2;
+						endIndex = x;
+						break;
+					}
+				} else if (tempChar == '(' || tempChar == ')') {
+					if (!bracket.empty() && bracket.peek() != tempChar) {
+						bracket.pop();
+					} else {
+						bracket.push(tempChar);
+					}
+				} else if (tempChar != ' ' && tempChar != '(' && tempChar != ')' && bracket.empty()) {
+					stringBuilder.append(tempChar);
+				}
+			}
 		}
-		return newSubQuery.toString();
+		if (operator.equals("and")) {
+			return andInfix(query.substring(0, startIndex), query.substring(endIndex));
+		} else if (operator.equals("or")) {
+			return orInfix(query.substring(0, startIndex), query.substring(endIndex));
+		} else if (operator.equals("not")) {
+			return notInfix(query.substring(endIndex));
+		} else if (operator.equals("atomic")) {
+			return query;
+		} else {
+			System.out.println("Error");
+			return null;
+		}
 	}
 
+	public static String andInfix(String left, String right) {
+		String leftQuery = parseInfix(left);
+		String rightQuery = parseInfix(right);
+
+		return "and(" + leftQuery + "," + rightQuery + ")";
+	}
+
+	public static String orInfix(String left, String right) {
+		String leftQuery = parseInfix(left);
+		String rightQuery = parseInfix(right);
+		return "or(" + leftQuery + "," + rightQuery + ")";
+	}
+
+	public static String notInfix(String right) {
+		String rightQuery = parseInfix(right);
+		return "not(" + rightQuery + ")";
+	}
+
+	// recursive method
+	public static String parseCoveringBracket(String q) {
+		String thisQuery = q;
+		if (thisQuery.length() > 0 && thisQuery.charAt(0) == '(' && thisQuery.charAt(thisQuery.length() - 1) == ')') {
+			thisQuery = thisQuery.substring(1, thisQuery.length() - 1);
+			if (thisQuery.length() > 0 && thisQuery.charAt(0) == '('
+					&& thisQuery.charAt(thisQuery.length() - 1) == ')') {
+				thisQuery = parseCoveringBracket(thisQuery);
+			}
+		}
+		return thisQuery;
+	}
 }
