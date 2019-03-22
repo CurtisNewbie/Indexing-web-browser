d78daddd07ea3819f43b60df40b58a82663eea94
Curtis <Zhuangyongj@gmail.com>
2019-03-22 17:21:54 +0000

    Documenting the code

--- IndexingWebBrowser/src/AndQuery.java
+++ IndexingWebBrowser/src/AndQuery.java
@@ -6,22 +6,61 @@ import java.util.TreeSet;
 
 import javafx.animation.Interpolator;
 
+/**
+ * This class is used to handle the prefix AndQuery, e.g., and(banana,apple). It
+ * is part of the recursion that the matches() method will call the
+ * QueryBuilder.parse() to parse the sub-query of this AndQuery object, the
+ * sub-query can be an AndQuqery object as well.
+ * 
+ * @author 180139796
+ *
+ */
 public class AndQuery implements Query {
 
-	private TreeSet<String> queryStr;
+	/**
+	 * It represents the sub-query of this AndQuery, it can be more than two in case
+	 * of the prefix form.
+	 */
+	private TreeSet<String> subQuery;
+
+	/**
+	 * The normal query refers to the query that is not NotQuery (AndQuery, OrQuery,
+	 * AtomicQuery). The result refers to the result of the matches() of the
+	 * sub-query. Thus, it's a collection of results of the 'normal' query.
+	 */
 	private ArrayList<Set<WebDoc>> normalQueryResult;
+
+	/**
+	 * It is a collection of results of the matches() of the NotQuery.
+	 */
 	private ArrayList<Set<WebDoc>> notQueryResult;
 
-	public AndQuery(TreeSet<String> SubQuery) {
-		queryStr = SubQuery;
+	/**
+	 * Constructor that initialises the instance variables(normalQueryResult,
+	 * notQueryResult and subQuery).
+	 * 
+	 * @param subQuery a TreeSet<String> of sub-query.
+	 */
+	public AndQuery(TreeSet<String> subQuery) {
+		this.subQuery = subQuery;
 		normalQueryResult = new ArrayList<>();
 		notQueryResult = new ArrayList<>();
 	}
 
-	@Override
+	/**
+	 * This method search through the given WebIndex based on the query to find all
+	 * the matched results. The AndQuery finds the common parts of each sub-queries
+	 * except the NotQuery, if there is NotQuery, the final result should exclude
+	 * the results of the NotQuery.
+	 * 
+	 * @return a Set<WebDoc> that is found based on the query and the given
+	 *         WebIndex.
+	 * @param wind the WebIndex that is used to search through based on the query.
+	 * @Override
+	 */
 	public Set<WebDoc> matches(WebIndex wind) {
 		// Get the results of all the sub-queries
-		for (String eachQuery : queryStr) {
+		for (String eachQuery : subQuery) {
 			Query subQuery = QueryBuilder.parse(eachQuery);
 			if (subQuery instanceof NotQuery) {
 				notQueryResult.add(subQuery.matches(wind));
@@ -29,21 +68,25 @@ public class AndQuery implements Query {
 				normalQueryResult.add(subQuery.matches(wind));
 			}
 		}
-
-		// Retain all the common elements for the sub-queries that
-		// are not NotQuery
+		/*
+		 * Retain all the common elements for the sub-queries that are not NotQuery, and
+		 * put them into the finalNormalQueryResult.
+		 */
 		Set<WebDoc> finalNormalQueryResult = new TreeSet<>();
 		finalNormalQueryResult = normalQueryResult.get(0);
 		for (Set<WebDoc> eachSet : normalQueryResult) {
 			if (eachSet != null) {
 				finalNormalQueryResult.retainAll(eachSet);
-			} else { // one of the result is null, then the result should be null as well.
+			} else { // If one of the result is null, then the final result should be null as well.
 				finalNormalQueryResult = null;
 				break;
 			}
 		}
-
-		// Retain all the common elements for all the NotQuery
+		/*
+		 * If the finalNormalQueryResult is not null, it retain all the common elements
+		 * for all the NotQuery and find the difference between the
+		 * finalNormalQueryResult and the finalNotQueryResult.
+		 */
 		Set<WebDoc> finalNotQueryResult = new TreeSet<>();
 		if (!notQueryResult.isEmpty() && finalNormalQueryResult != null) {
 			// Put all the Sets of NotQuery together.
@@ -57,10 +100,18 @@ public class AndQuery implements Query {
 		return finalNormalQueryResult;
 	}
 
+	/**
+	 * This method returns a String indicate the type of the query and the
+	 * sub-query.The sub-query is indicated using '[' and ']'. 
+	 * E.g., and(A,and(C,D) -> AND([A],[and(C,D)])
+	 * 
+	 * @return a string that indicates the type of this query as well as the
+	 *         sub-query of this query.
+	 */
 	@Override
 	public String toString() {
 		StringBuilder stringBuilder = new StringBuilder("AND(");
-		Iterator<String> eachQuery = queryStr.iterator();
+		Iterator<String> eachQuery = subQuery.iterator();
 
 		while (eachQuery.hasNext()) {
 			stringBuilder.append("[" + eachQuery.next() + "]");
