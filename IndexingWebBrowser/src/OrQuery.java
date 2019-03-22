1cace0ebe2bc9e0e4afd3f87efb7e391b3556b29
Curtis <Zhuangyongj@gmail.com>
2019-03-22 17:40:09 +0000

    Documenting the code

--- IndexingWebBrowser/src/OrQuery.java
+++ IndexingWebBrowser/src/OrQuery.java
@@ -5,24 +5,48 @@ import java.util.TreeSet;
 
 public class OrQuery implements Query {
 
-	private TreeSet<String> queryStr;
+	/**
+	 * It represents the sub-query of this AndQuery, it can be more than two in case
+	 * of the prefix form.
+	 */
+	private TreeSet<String> subQuery;
+
+	/**
+	 * It is a collection of results of the matches() of all the sub-query.
+	 */
 	private ArrayList<Set<WebDoc>> subQueryResult;
 
-	public OrQuery(TreeSet<String> queryCol) {
-		queryStr = queryCol;
+	/**
+	 * Constructor of AndQuery that initialises the instance variables(subQuery and
+	 * subQueryResult).
+	 * 
+	 * @param subQuery a TreeSet<String> of sub-query.
+	 */
+	public OrQuery(TreeSet<String> subQuery) {
+		this.subQuery = subQuery;
 		subQueryResult = new ArrayList<>();
 	}
 
+	/**
+	 * This method search through the given WebIndex based on the query to find all
+	 * the matched results. The OrQuery object finds the matched results of all the
+	 * sub-query and puts them together. As it is OrQuery, the notQuery is ignored,
+	 * as it will not affect the final result.
+	 * 
+	 * @return a Set<WebDoc> that is found based on the query and the given
+	 *         WebIndex.
+	 * @param wind the WebIndex that is used to search through based on the query.
+	 * 
+	 */
 	@Override
 	public Set<WebDoc> matches(WebIndex wind) {
 		// Get the results of all the sub-queries
-		for (String eachQuery : queryStr) {
+		for (String eachQuery : subQuery) {
 			Query subQuery = QueryBuilder.parse(eachQuery);
 			if (!(subQuery instanceof NotQuery)) { // In OrQuery, the NotQuery is not important.
 				subQueryResult.add(subQuery.matches(wind));
 			}
 		}
-
 		Set<WebDoc> finalSubQueryResult = new TreeSet<>();
 		for (Set<WebDoc> eachSet : subQueryResult) {
 			if (eachSet != null) {
@@ -32,13 +56,22 @@ public class OrQuery implements Query {
 		return finalSubQueryResult;
 	}
 
+	/**
+	 * This method returns a String indicate the type of the query and the
+	 * sub-query.The sub-query is indicated using '[' and ']'. E.g., or(A,and(C,D)
+	 * -> OR([A],[and(C,D)])
+	 * 
+	 * @return a string that indicates the type of this query as well as the
+	 *         sub-query of this query.
+	 */
+	@Override
 	public String toString() {
 		StringBuilder stringBuilder = new StringBuilder("OR(");
-		Iterator<String> eachQuery = queryStr.iterator();
+		Iterator<String> eachQuery = subQuery.iterator();
 
 		while (eachQuery.hasNext()) {
 			stringBuilder.append("[" + eachQuery.next() + "]");
-			if(eachQuery.hasNext()) {
+			if (eachQuery.hasNext()) {
 				stringBuilder.append(",");
 			}
 		}
