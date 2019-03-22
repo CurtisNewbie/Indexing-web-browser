ad1b80ee3689bc8cff71240639ed8766e1a6f014
Curtis <Zhuangyongj@gmail.com>
2019-03-22 00:49:14 +0000

    Changed the method name and fixed the identification of NotQuery.
    The method name has been changed to reduce ambiguity.
    The identification of NotQuery has be optimised with Regex.

--- IndexingWebBrowser/src/QueryBuilder.java
+++ IndexingWebBrowser/src/QueryBuilder.java
@@ -5,8 +5,6 @@ import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.sun.prism.impl.shape.OpenPiscesRasterizer;
-
 public class QueryBuilder {
 
 	public static Query parse(String q) {
@@ -62,84 +60,87 @@ public class QueryBuilder {
 	}
 
 	public static Query parseInfixForm(String q) {
-		String prefixQuery = parseInfix(q);
+		String prefixQuery = parseInfixString(q);
 		return parse(prefixQuery);
 	}
 
-	private static String parseInfix(String str) {
+	private static String parseInfixString(String str) {
 		String query = str.trim();
-		query = parseCoveringBracket(query);
+		query = parseCoveringBracket(query); // get rid of all the outside closing bracket.
 		Stack<Character> bracket = new Stack<>();
-		String operator = "atomic";
+		String operator = "default"; 
 		int startIndex = 0;
 		int endIndex = 0;
 		StringBuilder stringBuilder = new StringBuilder();
 
-		if (query.startsWith("not") && !query.contains("and") && !query.contains("or")) {
+		if (query.matches("not\\s+\\(.+\\)")) { // NotQuery
 			operator = "not";
 			endIndex = 3;
-		} else if (!query.contains("and") && !query.contains("not") && !query.contains("or")) {
-			operator = "atomic";
+		} else if (!query.contains("and") && !query.contains("not") && !query.contains("or")
+				|| query.matches("(and\\(.+,.+\\))|(or\\(.+,.+\\))")) {
+			operator = "atomic"; // AtomicQuery
 		} else {
 			for (int x = 0; x < query.length(); x++) {
 				char tempChar = query.charAt(x);
 				if (tempChar == ' ' && bracket.isEmpty()) {
-					if (stringBuilder.toString().contains("and")) {
+					if (stringBuilder.toString().contains("and")) { // AndQuery
 						operator = "and";
 						startIndex = x - 3;
 						endIndex = x;
 						break;
-					} else if (stringBuilder.toString().contains("or")) {
+					} else if (stringBuilder.toString().contains("or")) { // OrQuery
 						operator = "or";
 						startIndex = x - 2;
 						endIndex = x;
 						break;
 					}
-				} else if (tempChar == '(' || tempChar == ')') {
+				} else if (tempChar == '(' || tempChar == ')') { // see whether the current char is within the brackets
 					if (!bracket.empty() && bracket.peek() != tempChar) {
 						bracket.pop();
 					} else {
 						bracket.push(tempChar);
 					}
 				} else if (tempChar != ' ' && tempChar != '(' && tempChar != ')' && bracket.empty()) {
+					// only check the next operator that is outside the bracket
 					stringBuilder.append(tempChar);
 				}
 			}
 		}
+		// check what type of query it is.
 		if (operator.equals("and")) {
-			return andInfix(query.substring(0, startIndex), query.substring(endIndex));
+			return parseAndInfix(query.substring(0, startIndex), query.substring(endIndex));
 		} else if (operator.equals("or")) {
-			return orInfix(query.substring(0, startIndex), query.substring(endIndex));
+			return parseOrInfix(query.substring(0, startIndex), query.substring(endIndex));
 		} else if (operator.equals("not")) {
-			return notInfix(query.substring(endIndex));
+			return parseNotInfix(query.substring(endIndex));
 		} else if (operator.equals("atomic")) {
 			return query;
-		} else {
-			System.out.println("Error");
+		} else { // it is for debugging, normal situation it should never occur
+			System.out.println("Incorrect Format!");
+			System.out.println(query);
 			return null;
 		}
 	}
 
-	public static String andInfix(String left, String right) {
-		String leftQuery = parseInfix(left);
-		String rightQuery = parseInfix(right);
-
+	private static String parseAndInfix(String left, String right) {
+		String leftQuery = parseInfixString(left);
+		String rightQuery = parseInfixString(right);
 		return "and(" + leftQuery + "," + rightQuery + ")";
 	}
 
-	public static String orInfix(String left, String right) {
-		String leftQuery = parseInfix(left);
-		String rightQuery = parseInfix(right);
+	private static String parseOrInfix(String left, String right) {
+		String leftQuery = parseInfixString(left);
+		String rightQuery = parseInfixString(right);
 		return "or(" + leftQuery + "," + rightQuery + ")";
 	}
 
-	public static String notInfix(String right) {
-		String rightQuery = parseInfix(right);
+	private static String parseNotInfix(String right) {
+		String rightQuery = parseInfixString(right);
 		return "not(" + rightQuery + ")";
 	}
 
 	// recursive method
-	public static String parseCoveringBracket(String q) {
+	private static String parseCoveringBracket(String q) {
 		String thisQuery = q;
 		if (thisQuery.length() > 0 && thisQuery.charAt(0) == '(' && thisQuery.charAt(thisQuery.length() - 1) == ')') {
 			thisQuery = thisQuery.substring(1, thisQuery.length() - 1);
