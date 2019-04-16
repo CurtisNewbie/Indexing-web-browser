62016ecebfc7f569b7461793b9ef035277acc3a7
Curtis <Zhuangyongj@gmail.com>
2019-04-16 12:50:58 +0100

    added a penel with CardLayout and a few listener for menuitems
    A panel using cardLayout is added as the top layer to the contentPane of
    the frame.
    
    Listeners are associated with the menu items in the menu bar, through
    which the navigation between the cards are accomplished.

--- IndexingWebBrowser/src/GraphicUserInterface/WebBrowserGUI.java
+++ IndexingWebBrowser/src/GraphicUserInterface/WebBrowserGUI.java
@@ -1,6 +1,7 @@
 package GraphicUserInterface;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -8,6 +9,10 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.smartcardio.ATR;
+import javax.smartcardio.Card;
+import javax.smartcardio.CardChannel;
+import javax.smartcardio.CardException;
 import javax.swing.JButton;
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
@@ -28,22 +33,38 @@ public class WebBrowserGUI {
 	// The browser as a whole
 	JFrame browserFrame;
 
-	// JTabbedPane as a contentPane of the browser
-	JTabbedPane browserContentPane;
-	
+	// The panel with CardLayout for showing different content
+	JPanel cards;
+
+	// CardLayout Manager for controlling the cards switching
+	CardLayout cardLayoutControl;
+
+	// The card for the web browser
+	JPanel webBrowserCard;
+
+	// The card for the query browser
+	JPanel queryBrowserCard;
+
+	// Tags for the two cards
+	final String WEB_BROWSER_TAG = "WebBrowserTag";
+	final String QUERY_BROWSER_TAG = "QueryBrowserTag";
+
+	// JTabbedPane as a contentPane of the webBrowserCard
+	JTabbedPane webBrowserContentPane;
+
 	// The navigation buttons in the menu bar
 	JMenuItem webBrowser;
 	JMenuItem queryBrowser;
-	
+
 	// Insets between navigation buttons in the menu bar
 	final int insetsTop = 5;
 	final int insetsBottom = 5;
 	final int insetsLeft = 5;
 	final int insetsRight = 5;
-	
+
 	// Default Menu Font
 	Font menuFont = new Font("Arial", Font.BOLD, 19);
-	
+
 	// Default Content Font
 	Font contentFont = new Font("Arial", Font.BOLD, 17);
 
@@ -53,20 +74,31 @@ public class WebBrowserGUI {
 		browserFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// Set properties
-		browserFrame.setVisible(true);
-		Dimension pcScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		browserFrame.setSize(pcScreenSize.width, pcScreenSize.height);
+//		Dimension pcScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
+//		browserFrame.setSize(pcScreenSize.width / 2, pcScreenSize.height / 2);
 
-		// add the menu bar to this frame 
+		// Use a panel (cards) with cardLayout as a way to switch different contents
+		cardLayoutControl = new CardLayout();
+		cards = new JPanel(cardLayoutControl);
+
+		// set up each card
+		webBrowserCard = new JPanel();
+		queryBrowserCard = new JPanel();
+		cards.add(webBrowserCard, WEB_BROWSER_TAG);
+		cards.add(queryBrowserCard, QUERY_BROWSER_TAG);
+
+		// add the panel (with card layout) to the contentPane of the frame.
+		browserFrame.getContentPane().add(cards);
+
+		// set up the tabbed pane for the webBrowserCard
+		webBrowserContentPane = new JTabbedPane();
+		webBrowserCard.add(webBrowserContentPane);
+		
+		// add the menu bar to this frame for navigating between cards
 		addMenuBar();
-		
-		// set up the contentPane
-		browserContentPane = new JTabbedPane();
-		browserContentPane.setLayout(new BorderLayout());
-		
-		browserFrame.getContentPane().add(browserContentPane);
-		
-//		addTabToBrowser();
+
+		browserFrame.pack();
+		browserFrame.setVisible(true);
 	}
 
 	private void addMenuBar() {
@@ -74,35 +106,41 @@ public class WebBrowserGUI {
 		menu = new JMenu("Menu");
 		menu.setBorder(new LineBorder(Color.BLACK));
 		menu.setFont(menuFont);
-		
+
+		// Create menu itme and set up their font
 		webBrowser = new JMenuItem("Web Browser");
 		webBrowser.setFont(menuFont);
 		queryBrowser = new JMenuItem("Query Browser");
 		queryBrowser.setFont(menuFont);
-		
+
 		menu.add(webBrowser);
 		menu.add(queryBrowser);
 		menuBar.add(menu);
 		browserFrame.add(menuBar, BorderLayout.NORTH);
-		
+
 		webBrowser.addActionListener(new ActionListener() {
-			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				addTabToBrowser();
+				cardLayoutControl.show(cards, WEB_BROWSER_TAG);
+			}
+		});
+
+		queryBrowser.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cardLayoutControl.show(cards, QUERY_BROWSER_TAG);
+
 			}
 		});
 	}
-	
-	private void addTabToBrowser() {
+
+	private void addTabToWebBrowserCard() {
 		JPanel panel = new JPanel();
-		panel.setBackground(Color.red);
-		browserContentPane.addTab("Web1", panel);
-		panel.add(new JButton("botton"));
+		webBrowserContentPane.addTab("new tab", panel);
 	}
 
 	public static void main(String[] args) {
 		WebBrowserGUI brow = new WebBrowserGUI();
-		
+
 	}
 }
