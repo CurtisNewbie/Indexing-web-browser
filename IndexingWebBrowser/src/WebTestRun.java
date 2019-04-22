d7638acf6633e58640bf7086e3610388ee283c5e
Curtis <Zhuangyongj@gmail.com>
2019-04-22 12:08:04 +0100

    This class is used to run the program with GUI.

---
+++ IndexingWebBrowser/src/WebTestRun.java
@@ -0,0 +1,16 @@
+
+/**
+ * This class is used to instantiate a new object of WebBrowserView as the view
+ * in MVC architecture and a new object of WebBrowserController as the
+ * controller in the MVC.
+ * 
+ * @author 180139796
+ *
+ */
+public class WebTestRun {
+
+	public static void main(String[] args) {
+		WebBrowserView view = new WebBrowserView();
+		WebBrowserController controller = new WebBrowserController(view);
+	}
+}
