7cf3bd06a1e90a9a97d6181418bfc03828f314ec
CurtisNewbie <zhuangyongj@gmail.com>
2019-12-14 18:48:45 +0000

    Add BrowserView class (not yet implemented)
    This class is the highest-level representation of the GUI of this program.
    
    new file:   IndexingWebBrowser/src/main/java/com/curtisnewbie/view/BrowserView.java

---
+++ IndexingWebBrowser/src/main/java/com/curtisnewbie/view/BrowserView.java
@@ -0,0 +1,47 @@
+package com.curtisnewbie.view;
+
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+/**
+ * <p>
+ * The Highest level representation of the view in MVC. This class provides
+ * methods to get information from and update status of the various components
+ * stored.
+ * </p>
+ * <p>
+ * This program has two views (or cards of CardLayout) in Javafx, one is for
+ * viewing the content of the webpages, and another one is for using query to
+ * seaarch contents.
+ * </p>
+ * <p>
+ * This class itself is a subclass of Pane, and it controls switching between
+ * the two views by have two BorderPane as its instance variables.
+ * </p>
+ */
+public class BrowserView extends BorderPane {
+
+    /**
+     * A Pane consits of a group of nodes/componenets for the layout displaying the
+     * HTML viewing UI
+     */
+    private BorderPane displayPane;
+
+    /**
+     * A Pane consits of group of nodes/componenets for the layout displaying the
+     * query searching UI
+     */
+    private BorderPane queryGroup;
+
+    public BrowserView() {
+        init();
+    }
+
+    /** Initialise The View including its' internal components */
+    private void init() {
+
+        displayPane = new DisplayPane();
+        this.setCenter(displayPane);
+    }
+
+}
