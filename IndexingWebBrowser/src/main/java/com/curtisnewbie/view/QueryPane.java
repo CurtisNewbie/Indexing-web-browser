d9b1f954c9f97a54bf203a676a775e6e717529dd
CurtisNewbie <zhuangyongj@gmail.com>
2019-12-15 16:08:02 +0000

    Implement HistoryPanel in QueryPane
    HistoryPanel is a VBox used to display the history of the browsed webpages. Each webpage (URL
    links) shown in this VBox is a clickable button.
    
    modified:   IndexingWebBrowser/src/main/java/com/curtisnewbie/view/QueryPane.java

--- IndexingWebBrowser/src/main/java/com/curtisnewbie/view/QueryPane.java
+++ IndexingWebBrowser/src/main/java/com/curtisnewbie/view/QueryPane.java
@@ -1,9 +1,15 @@
 package com.curtisnewbie.view;
 
+import javafx.scene.control.ScrollPane;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.Node;
+import javafx.collections.ObservableList;
 import javafx.geometry.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.image.*;
+import javafx.scene.text.Text;
 import java.io.*;
 
 public class QueryPane extends BorderPane {
@@ -23,10 +29,15 @@ public class QueryPane extends BorderPane {
      */
     private Button menuBtn;
 
-    public QueryPane() {
-        // load label for the menuBtn button
-        ClassLoader loader = this.getClass().getClassLoader();
+    /** Control Panel for entering queries and starting query searching */
+    private QueryControlPanel queryControlPanel;
 
+    /** Panel for displaying browsing history */
+    private HistoryPanel historyPanel;
+
+    public QueryPane() {
+        // load image icon for the menuBtn button
+        ClassLoader loader = this.getClass().getClassLoader();
         InputStream menuIn = loader.getResourceAsStream(PATH_TO_MENUICON);
         if (menuIn == null)
             throw new IllegalArgumentException("Cannot find Icon Image at \"" + PATH_TO_MENUICON + "\"");
@@ -39,9 +50,54 @@ public class QueryPane extends BorderPane {
         BorderPane.setAlignment(menuBtn, Pos.CENTER_RIGHT);
 
         // for testing only
-        this.setCenter(new QueryControlPanel());
+        this.setCenter(new HistoryPanel());
+    }
+}
+
+/**
+ * VBox that displays the history of the browsed webpages. Each webpage (URL
+ * links) shown in this VBox is a clickable button.
+ */
+class HistoryPanel extends VBox {
+
+    private final String title = "History:  ";
+    private VBox historiesVBox;
+    private ObservableList<Node> observableList;
+
+    public HistoryPanel() {
+        historiesVBox = new VBox();
+        observableList = historiesVBox.getChildren();
+        this.getChildren().addAll(new Text(title), new ScrollPane(historiesVBox));
+        /*
+         * --------------------------------------------------------------------------
+         * 
+         * for testing
+         * 
+         * --------------------------------------------------------------------------
+         */
+        var testList = new ArrayList<String>();
+        for (int i = 0; i < 30; i++)
+            testList.add("https://www.google.com");
+        refresh(testList);
     }
 
+    /**
+     * Refresh this history panel by clearing the original {@code ObservableList}
+     * and reloading the strings (each in a {@code Button}) into this {@code
+     * ObservableList}
+     * 
+     * @param urls A List of url strings
+     * 
+     */
+    public void refresh(List<String> urls) {
+        if (urls != null && urls.size() > 0) {
+            observableList.clear();
+            for (String url : urls) {
+                var btn = new Button(url);
+                observableList.add(btn);
+            }
+        }
+    }
 }
 
 /**
