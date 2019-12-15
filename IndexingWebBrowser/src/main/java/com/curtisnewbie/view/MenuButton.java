7558b46d46faeffbf9185a29e37e6861ea2476bf
CurtisNewbie <zhuangyongj@gmail.com>
2019-12-15 18:42:25 +0000

    Implement MenuButton
    Previous Button was used to represent the "Menu" in this program for switching the view between the DisplayPane and the QueryPane. Both "views" need to have this Menu, so this class implements this feature for both views to use directly.
    
    new file:   IndexingWebBrowser/src/main/java/com/curtisnewbie/view/MenuButton.java

---
+++ IndexingWebBrowser/src/main/java/com/curtisnewbie/view/MenuButton.java
@@ -0,0 +1,41 @@
+package com.curtisnewbie.view;
+
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.InputStream;
+
+import javafx.collections.*;
+
+public class MenuButton extends Menu {
+
+    /** Maximum height of icon images in the buttons */
+    private double MAX_IMG_HEIGHT = 20;
+
+    /** ObservableList of MenuItem under this menu */
+    private ObservableList<MenuItem> observableItems;
+
+    /** Path to the icon for menuBtn buttn */
+    private final String PATH_TO_MENUICON = "img/menu_icon.png";
+
+    public MenuButton() {
+        // load icon for this menu
+        ClassLoader loader = getClass().getClassLoader();
+        InputStream menuIn = loader.getResourceAsStream(PATH_TO_MENUICON);
+        if (menuIn == null)
+            throw new IllegalArgumentException("Cannot find Icon Image at \"" + PATH_TO_MENUICON + "\"");
+
+        ImageView menuIcon = new ImageView(new Image(menuIn));
+        menuIcon.setFitWidth(MAX_IMG_HEIGHT);
+        menuIcon.setFitHeight(MAX_IMG_HEIGHT);
+        this.setGraphic(menuIcon);
+
+        // add to MenuItems in this menu
+        this.observableItems = this.getItems();
+        MenuItem toDisplayPane = new MenuItem("DisplayPane");
+        MenuItem toQueryPane = new MenuItem("QueryPane");
+        this.observableItems.addAll(toDisplayPane, toQueryPane);
+    }
+
+}
