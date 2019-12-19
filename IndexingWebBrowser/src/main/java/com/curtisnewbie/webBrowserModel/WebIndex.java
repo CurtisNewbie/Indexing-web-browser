1e70c2744ee33d481db267ecb2adbd4258998486
CurtisNewbie <zhuangyongj@gmail.com>
2019-12-19 21:04:34 +0000

    Reimpl WebIndex, and make it an abstract class
    Subclasses can extend this class to modify the behaviour of adding new WebDoc.
    
    modified:   IndexingWebBrowser/src/main/java/com/curtisnewbie/webBrowserModel/WebIndex.java

--- IndexingWebBrowser/src/main/java/com/curtisnewbie/webBrowserModel/WebIndex.java
+++ IndexingWebBrowser/src/main/java/com/curtisnewbie/webBrowserModel/WebIndex.java
@@ -6,81 +6,46 @@ import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * It is a web index that can check the web documents and see which contains a
- * particular string. It also contains the keywords or content words extracted
- * from each web document. It can also provide overall statistics summary of
- * this WebIndex.
+ * It is a web index that contains a number of WebDoc. It can check the web
+ * documents stored and see which one contains a particular word.
  * 
- * @author 180139796
+ * @see WebDoc
+ * @see WebIndexForBody
+ * @see WebIndexForHead
+ * @author Yongjie Zhuang
  */
-public class WebIndex {
+public abstract class WebIndex {
 
 	/**
-	 * It refers to the type of words that the web index stored.
+	 * The 'keys' are the words, the 'values' are the Set of WebDoc.
 	 */
-	public enum TypeOfWords {
-		KEYWORD, CONTENT_WORD
-	}
-
-	/**
-	 * It indicates the type of words that the web index is trying to store.
-	 * (keywords or content words)
-	 */
-	private TypeOfWords typeofWords;
-
-	/**
-	 * The 'keys' are the words (content words or keywords), the 'values' are the
-	 * Set of WebDoc.
-	 */
-	private Map<String, Set<WebDoc>> webDocsMap;
+	protected Map<String, Set<WebDoc>> webDocsMap;
 
 	/**
 	 * Number of WebDoc in this web index.
 	 */
-	private int numOfDocs;
+	protected int numOfDocs;
 
 	/**
-	 * Number of words (keywords or content words) read from these WebDoc.
+	 * Number of words read from these WebDoc.
 	 */
-	private int numOfWords;
+	protected int numOfWords;
 
 	/**
 	 * Initialise the webDocs. Assign 0 to numOfDocs and numOfWord.
 	 */
-	public WebIndex(TypeOfWords type) {
-		this.typeofWords = type;
+	public WebIndex() {
 		webDocsMap = new HashMap<>();
 		this.numOfDocs = 0;
 		this.numOfWords = 0;
 	}
 
 	/**
-	 * Add an object of WebDoc into the index and increment numOfDocs by one. Assign
-	 * the size of the map to the numOfWords.
+	 * Add an object of WebDoc into the index and increment numOfDocs by one.
 	 * 
 	 * @param doc An object of WebDoc
 	 */
-	public void add(WebDoc doc) {
-		Set<String> tempWordSet;
-		if (typeofWords == TypeOfWords.CONTENT_WORD) {
-			tempWordSet = doc.getContentWords();
-		} else {
-			tempWordSet = doc.getKeywords();
-		}
-		if (!tempWordSet.isEmpty()) {
-			for (String word : tempWordSet) {
-				Set<WebDoc> newSet = new TreeSet<WebDoc>();
-				newSet.add(doc);
-				Set<WebDoc> temp = webDocsMap.putIfAbsent(word, newSet); // return null if the key is already associated
-																			// with a key
-				if (temp != null) { // If the key is already associated with a value.
-					temp.add(doc);
-				}
-			}
-		}
-		numOfDocs++;
-		numOfWords = webDocsMap.size();
-	}
+	public abstract void add(WebDoc doc);
 
 	/**
 	 * This method searches through the web index and finds the results that match
@@ -113,17 +78,12 @@ public class WebIndex {
 	}
 
 	/**
-	 * Get a string that provide the overall statistics summary of this WebIndex
+	 * Get a string that provide the overall summary of this WebIndex
 	 * 
-	 * @return a string of the overall statistics summary of this WebIndex.
+	 * @return a string of the overall summary of this WebIndex.
 	 */
 	@Override
 	public String toString() {
-
-		if (typeofWords == TypeOfWords.KEYWORD) {
-			return "WebIndex over keywords contains " + numOfWords + " from " + numOfDocs + " documents";
-		} else {
-			return "WebIndex over contents contains " + numOfWords + " from " + numOfDocs + " documents";
-		}
+		return "WebIndex contains " + numOfWords + " from " + numOfDocs + " documents";
 	}
 }
