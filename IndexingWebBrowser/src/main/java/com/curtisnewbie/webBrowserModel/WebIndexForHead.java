30a30612bd283cdc8400ee39a93615cd3d2726bd
CurtisNewbie <zhuangyongj@gmail.com>
2019-12-19 21:33:31 +0000

    Implement WebIndexForHead that extends WebIndex
    new file:   IndexingWebBrowser/src/main/java/com/curtisnewbie/webBrowserModel/WebIndexForHead.java

---
+++ IndexingWebBrowser/src/main/java/com/curtisnewbie/webBrowserModel/WebIndexForHead.java
@@ -0,0 +1,26 @@
+package com.curtisnewbie.webBrowserModel;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class WebIndexForHead extends WebIndex {
+
+    @Override
+    public void add(WebDoc doc) {
+        Set<String> tempWordSet;
+        tempWordSet = doc.getHeadWords();
+        if (!tempWordSet.isEmpty()) {
+            for (String word : tempWordSet) {
+                Set<WebDoc> newSet = new TreeSet<WebDoc>();
+                newSet.add(doc);
+                Set<WebDoc> temp = webDocsMap.putIfAbsent(word, newSet);
+                if (temp != null) {
+                    // If the key is already associated with a set, add the doc to this set
+                    temp.add(doc);
+                }
+            }
+        }
+        numOfDocs++;
+        numOfWords = webDocsMap.size();
+    }
+}
