d167967300a34ae26288ad34cf83766363d40341
CurtisNewbie <zhuangyongj@gmail.com>
2019-12-21 19:43:31 +0000

    Rename vboxs, remove ObservableList and methods for refreshing
    - Rename to make them more meaningful
    	hdRes -> hdResVBox
    	bdRes -> bdResVbox
    - Remove ObservableList and methods for refreshing the "view", as ObservableList should be obtained by using .getChildren() method. Previous way to storing the ObservableList as an instance variable can cause exceptions.
    - Add getters for the two VBoxs
    
    modified:   IndexingWebBrowser/src/main/java/com/curtisnewbie/view/QueryResultPanel.java

--- IndexingWebBrowser/src/main/java/com/curtisnewbie/view/QueryResultPanel.java
+++ IndexingWebBrowser/src/main/java/com/curtisnewbie/view/QueryResultPanel.java
@@ -3,6 +3,7 @@ package com.curtisnewbie.view;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
@@ -40,31 +41,23 @@ public class QueryResultPanel extends GridPane {
     private Label bodyLabel;
 
     /** Display Head results */
-    private VBox hdRes;
+    private VBox hdResVBox;
 
     /** Display Body results */
-    private VBox bdRes;
-
-    /** {@code ObservableList} for {@code VBox hdRes} */
-    private ObservableList<Node> hdResList;
-
-    /** {@code ObservableList} for {@code VBox bdRes} */
-    private ObservableList<Node> bdResList;
+    private VBox bdResVBox;
 
     public QueryResultPanel() {
         panelLabel = new Label(PANEL_TITLE);
         panelLabel.setFont(new Font(PANEL_TITLE_FONT));
         headLabel = new Label(HEAD_LABEL_STR);
         bodyLabel = new Label(BODY_LABEL_STR);
-        hdRes = new VBox();
-        bdRes = new VBox();
-        hdResList = hdRes.getChildren();
-        bdResList = bdRes.getChildren();
+        hdResVBox = new VBox();
+        bdResVBox = new VBox();
 
         // create and add two VBox to seperate the results for Head and Body
         var topBox = new VBox();
         topBox.setSpacing(VBOX_SPACING);
-        ScrollPane topBoxSP = new ScrollPane(hdRes);
+        ScrollPane topBoxSP = new ScrollPane(hdResVBox);
         topBoxSP.setStyle("-fx-background-color: transparent; -fx-control-inner-background: transparent;");
         topBoxSP.setFitToHeight(true);
         topBoxSP.setFitToWidth(true);
@@ -74,7 +67,7 @@ public class QueryResultPanel extends GridPane {
 
         var bottomBox = new VBox();
         bottomBox.setSpacing(VBOX_SPACING);
-        ScrollPane bottomBoxSP = new ScrollPane(bdRes);
+        ScrollPane bottomBoxSP = new ScrollPane(bdResVBox);
         bottomBoxSP.setStyle("-fx-background-color: transparent; -fx-control-inner-background: transparent;");
         bottomBoxSP.setFitToHeight(true);
         bottomBoxSP.setFitToWidth(true);
@@ -92,57 +85,25 @@ public class QueryResultPanel extends GridPane {
         this.getRowConstraints().addAll(row1, row2);
         this.getColumnConstraints().add(col1);
         this.setPadding(new Insets(PANEL_PADDING_TOP, PANEL_PADDING_RIGHT, PANEL_PADDING_BOTTOM, PANEL_PADDING_LEFT));
-
-        /*
-         * --------------------------------------------------------------------------
-         *
-         * for testing
-         *
-         * --------------------------------------------------------------------------
-         */
-        var testList = new ArrayList<String>();
-        for (int i = 0; i < 5; i++)
-            testList.add("https://www.google.com");
-        refreshBdRes(testList);
-        for (int i = 0; i < 15; i++)
-            testList.add("https://www.google.com");
-        refreshHdRes(testList);
     }
 
     /**
-     * Refresh Head query results by clearing the original
-     * {@code ObservableList<Node> hdResList} and reloading the strings (each in a
-     * {@code Button}) into this {@code ObservableList}
+     * Get Vbox for displaying head result
      * 
-     * @param urls A List of url strings
-     * 
+     * @return Vbox for displaying head result
      */
-    public void refreshHdRes(List<String> urls) {
-        if (urls != null) {
-            hdResList.clear();
-            for (String url : urls) {
-                var btn = new Button(url);
-                hdResList.add(btn);
-            }
-        }
+    public VBox getHdResVBox() {
+        return hdResVBox;
     }
 
     /**
-     * Refresh Body query results by clearing the original
-     * {@code ObservableList<Node> bdResList} and reloading the strings (each in a
-     * {@code Button}) into this {@code ObservableList}
+     * Get Vbox for displaying body result
      * 
-     * @param urls A List of url strings
-     * 
+     * @return Vbox for displaying body result
      */
-    public void refreshBdRes(List<String> urls) {
-        if (urls != null) {
-            bdResList.clear();
-            for (String url : urls) {
-                var btn = new Button(url);
-                bdResList.add(btn);
-            }
-        }
+
+    public VBox getBdResVBox() {
+        return bdResVBox;
     }
 
 }
