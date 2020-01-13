83aeb100b4f65cacfc4dc9f9d40c08166e75bd8d
CurtisNewbie <zhuangyongj@gmail.com>
2020-01-13 15:55:04 +0000

    Rename test method parsedQueryShouldBeCorrect() to parsedPrefixQueryShouldBeCorrect()
    modified:   IndexingWebBrowser/src/test/java/com/curtisnewbie/webBrowserModel/QueryBuilderTest.java

--- IndexingWebBrowser/src/test/java/com/curtisnewbie/webBrowserModel/QueryBuilderTest.java
+++ IndexingWebBrowser/src/test/java/com/curtisnewbie/webBrowserModel/QueryBuilderTest.java
@@ -5,15 +5,15 @@ import org.junit.Test;
 
 public class QueryBuilderTest {
 
-    @Test
-    public void parsedQueryShouldBeCorrect() {
-        Assert.assertTrue("Query \"and(apple,orange)\" should be parsed to \"AND([apple],[orange])\"",
-                QueryBuilder.parse("and(apple,orange)").toString().equals("AND([apple],[orange])"));
-        Assert.assertTrue("Query \"or(apple,orange)\" should be parsed to \"OR([apple],[orange])\"",
-                QueryBuilder.parse("or(apple,orange)").toString().equals("OR([apple],[orange])"));
-        Assert.assertTrue("Query \"not(apple)\" should be parsed to \"NOT([apple])\"",
-                QueryBuilder.parse("not(apple)").toString().equals("NOT([apple])"));
-        Assert.assertTrue("\"apple\" should be parsed to be an AtomicQuery object",
-                QueryBuilder.parse("apple") instanceof AtomicQuery);
-    }
+	@Test
+	public void parsedPrefixQueryShouldBeCorrect() {
+		Assert.assertTrue("Query \"and(apple,orange)\" should be parsed to \"AND([apple],[orange])\"",
+				QueryBuilder.parse("and(apple,orange)").toString().equals("AND([apple],[orange])"));
+		Assert.assertTrue("Query \"or(apple,orange)\" should be parsed to \"OR([apple],[orange])\"",
+				QueryBuilder.parse("or(apple,orange)").toString().equals("OR([apple],[orange])"));
+		Assert.assertTrue("Query \"not(apple)\" should be parsed to \"NOT([apple])\"",
+				QueryBuilder.parse("not(apple)").toString().equals("NOT([apple])"));
+		Assert.assertTrue("\"apple\" should be parsed to be an AtomicQuery object",
+				QueryBuilder.parse("apple") instanceof AtomicQuery);
+	}
 }
