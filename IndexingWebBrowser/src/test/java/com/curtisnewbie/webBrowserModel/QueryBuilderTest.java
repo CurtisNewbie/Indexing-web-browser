e77999b816a18dd664f0e1e9579191e30498532a
CurtisNewbie <zhuangyongj@gmail.com>
2020-01-14 00:24:46 +0000

    Add basic test cases for parsing infix queries
    modified:   IndexingWebBrowser/src/test/java/.../webBrowserModel/QueryBuilderTest.java

--- IndexingWebBrowser/src/test/java/com/curtisnewbie/webBrowserModel/QueryBuilderTest.java
+++ IndexingWebBrowser/src/test/java/com/curtisnewbie/webBrowserModel/QueryBuilderTest.java
@@ -16,4 +16,18 @@ public class QueryBuilderTest {
 		Assert.assertTrue("\"apple\" should be parsed to be an AtomicQuery object",
 				QueryBuilder.parse("apple") instanceof AtomicQuery);
 	}
+
+	@Test
+	public void parsedInfixQueryShouldBeCorrect() {
+		Assert.assertTrue("\"apple\" should be parsed to be an AtomicQuery object",
+				QueryBuilder.parseInfixForm("apple") instanceof AtomicQuery);
+		Assert.assertTrue("Query \"apple and orange\" should be parsed to \"AND([apple],[orange])\"",
+				QueryBuilder.parseInfixForm("apple and orange").toString().equals("AND([apple],[orange])"));
+		Assert.assertTrue("Query \"apple or orange\" should be parsed to \"OR([apple],[orange])\"",
+				QueryBuilder.parseInfixForm("apple or orange").toString().equals("OR([apple],[orange])"));
+		Assert.assertTrue("Query \"not(apple)\" should be parsed to \"NOT([apple])\"",
+				QueryBuilder.parseInfixForm("not(apple)").toString().equals("NOT([apple])"));
+		Assert.assertTrue("Query \"not apple \" should be parsed to \"NOT([apple])\"",
+				QueryBuilder.parseInfixForm("not apple").toString().equals("NOT([apple])"));
+	}
 }
