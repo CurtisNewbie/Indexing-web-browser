7e183d9a3d07f12cec1eebc8106d297153100b4c
CurtisNewbie <zhuangyongj@gmail.com>
2020-01-17 10:55:52 +0000

    Add the utility class (ConfigLoader) to load and parse config.json
    new file:   IndexingWebBrowser/src/main/java/com/curtisnewbie/config/ConfigLoader.java

---
+++ IndexingWebBrowser/src/main/java/com/curtisnewbie/config/ConfigLoader.java
@@ -0,0 +1,36 @@
+package com.curtisnewbie.config;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+
+public class ConfigLoader {
+
+    private final String CONFIG_PATH = "config.json";
+    private ClassLoader classLoader;
+
+    public ConfigLoader() {
+        this.classLoader = this.getClass().getClassLoader();
+    }
+
+    public Map<String, String> loadConfigFile() {
+
+        var in = classLoader.getResourceAsStream(CONFIG_PATH);
+        try {
+            JsonParser parser = new JsonFactory().createParser(in);
+            Map<String, String> map = new HashMap<>(); // may be modified in the future, it is now used for simple json
+                                                       // file.
+            while (parser.nextToken() != JsonToken.END_OBJECT) {
+                map.put(parser.getCurrentName(), parser.getText());
+            }
+            return map;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
