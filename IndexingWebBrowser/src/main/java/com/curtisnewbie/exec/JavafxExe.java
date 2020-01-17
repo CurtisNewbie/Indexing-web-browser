849c018db1eb77213a4e7c4adfebe00d10765495
CurtisNewbie <zhuangyongj@gmail.com>
2020-01-17 10:57:45 +0000

    Update the initialisation of Javafx program to load the title from config.json
    The parsed json file is also parsed to the controller for loading more configuration.
    
    modified:   IndexingWebBrowser/src/main/java/com/curtisnewbie/exec/JavafxExe.java

--- IndexingWebBrowser/src/main/java/com/curtisnewbie/exec/JavafxExe.java
+++ IndexingWebBrowser/src/main/java/com/curtisnewbie/exec/JavafxExe.java
@@ -7,6 +7,11 @@ import javafx.stage.Screen;
 import javafx.scene.Scene;
 
 import com.curtisnewbie.view.*;
+
+import java.util.Map;
+
+import com.curtisnewbie.config.Config;
+import com.curtisnewbie.config.ConfigLoader;
 import com.curtisnewbie.controller.*;
 
 /**
@@ -16,13 +21,29 @@ import com.curtisnewbie.controller.*;
  */
 public class JavafxExe extends Application {
 
+    private final String DEF_TITLE = "Simple Indexing Web Browser by Yongjie";
+    private Map<String, String> configMap;
+    private String title;
+
+    @Override
+    public void init() throws Exception {
+        super.init();
+        // load json config
+        ConfigLoader configLoader = new ConfigLoader();
+        Map<String, String> configMap = configLoader.loadConfigFile();
+        if (configMap != null) {
+            this.configMap = configMap;
+            this.title = configMap.get(Config.CONFIG_NAME);
+        }
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         BrowserView view = new BrowserView();
 
         // controller that controls the view
-        BrowserController controller = new BrowserController(view);
+        BrowserController controller = new BrowserController(view, configMap);
 
         // get screen size
         Rectangle2D screen = Screen.getPrimary().getBounds();
@@ -31,6 +52,7 @@ public class JavafxExe extends Application {
 
         Scene scene = new Scene(view, width * 0.7, height * 0.7);
         primaryStage.setScene(scene);
+        primaryStage.setTitle(title == null ? DEF_TITLE : title);
         primaryStage.show();
     }
 
